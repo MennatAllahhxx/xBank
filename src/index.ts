b0eed59d12222f104f4869c153c1d8028455e68f
Menna Hassan <h.menna85@gmail.com>
2025-03-09 21:43:26 +0200

    refactor:resolve coderabbit comments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
-const express = require('express');
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.get('/health', (req: Request, res: Response) => res.status(200).send('Healthy server'));
+app.get('/health', (req: Request, res: Response) => { res.status(200).send('Healthy server') });
 
-app.listen(port, () => console.log('app is running on port: ', port));
+app.listen(port, () => console.log(`app is running on port: ${port}`));
