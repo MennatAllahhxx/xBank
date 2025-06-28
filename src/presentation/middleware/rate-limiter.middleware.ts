5bdf0888c843167dcbc6488756f96ccf7c3d6a7f
Menna Hassan <h.menna85@gmail.com>
2025-06-28 22:19:10 +0300

    increased the max requestest count

--- src/presentation/middleware/rate-limiter.middleware.ts
+++ src/presentation/middleware/rate-limiter.middleware.ts
@@ -2,9 +2,13 @@ import rateLimit from "express-rate-limit";
 
 const rateLimiterConfig = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 1000,
     standardHeaders: true,
     legacyHeaders: false,
+    statusCode: 429,
+    message: {
+        error: "Too many requests from this IP, please try again later.",
+    }
 });
 
 export default rateLimiterConfig;
